1fc87cfa65eb7db41e56956dc7b6b43166aafd71
Andrea Gigli <andrea.gigli@dlr.de>
2020-09-04 00:06:01 +0200

    testing db obtained via moving window with a toy cnn

--- code/test_windowing.py
+++ code/test_windowing.py
@@ -2,17 +2,21 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
+from sklearn.datasets import make_regression
+import matplotlib.pyplot as plt
 
 # moving window using Dataset.from_generator
 # examples of how to use from_generator here https://sknadig.dev/TensorFlow2.0-dataset/
 # unfortunately, I found no reference of the moving window approach online, cannot
 # guarantee about the correctness of this approach.
-x = np.hstack((np.arange(1,10001,3)[:,None],
-               np.arange(2,10002,3)[:,None],
-               np.arange(3,10003,3)[:,None]))
-y = x[:,0:2]*10
-x.shape
-y.shape
+DATASET_SIZE = 10000
+x, y = make_regression(n_samples=DATASET_SIZE, n_features=3, n_targets=2, random_state=1)
+# x = np.hstack((np.arange(1,10001,3)[:,None],
+#                np.arange(2,10002,3)[:,None],
+#                np.arange(3,10003,3)[:,None]))
+# y = x[:,0:2]*10
+print(x.shape)
+print(y.shape)
 
 
 def createSlidingWindowGenerator(data,label,w):
@@ -36,26 +40,56 @@ def createSlidingWindowGenerator(data,label,w):
 # xy_gen = createSlidingWindowGenerator(x,y,3)
 # next(xy_gen)
 
+# dataset = tf.data.Dataset.from_generator(createSlidingWindowGenerator,
+#                                          output_types=(tf.float32, tf.float32),
+#                                          output_shapes=(tf.TensorShape([3,3]), tf.TensorShape([2])),
+#                                          args=[x,y,3])
+# iterator = dataset.as_numpy_iterator()
+# el = next(iterator)
+# print(el[0].shape)
+# print(el[1].shape)
+#
+# dataset_expanded = dataset.map(lambda x, y: (tf.expand_dims(x, axis=-1), y))
+# iterator = dataset_expanded.as_numpy_iterator()
+# el = next(iterator)
+# print(el[0].shape)
+# print(el[1].shape)
+#
+# dataset_expanded_shuffled = dataset_expanded.shuffle(1000)
+# iterator = dataset_expanded_shuffled.as_numpy_iterator()
+# el = next(iterator)
+# print(el[0].shape)
+# print(el[1].shape)
+#
+# dataset_expanded_shuffled_batched = dataset_expanded_shuffled.batch(128)
+# iterator = dataset_expanded_shuffled_batched.as_numpy_iterator()
+# el = next(iterator)
+# print(el[0].shape)
+# print(el[1].shape)
+
+
 dataset = tf.data.Dataset.from_generator(createSlidingWindowGenerator,
                                          output_types=(tf.float32, tf.float32),
                                          output_shapes=(tf.TensorShape([3,3]), tf.TensorShape([2])),
                                          args=[x,y,3])
-iterator = dataset.as_numpy_iterator()
-el = next(iterator)
-print(el[0].shape)
-print(el[1].shape)
+train_size = int(0.7 * DATASET_SIZE)
+val_size = int(0.2 * train_size) # 30% of the training set
+test_dataset = dataset.skip(train_size)
+train_dataset = dataset.take(train_size)
+val_dataset = train_dataset.take(val_size)
+train_dataset = train_dataset.skip(val_size)
 
-dataset_expanded = dataset.map(lambda x, y: (tf.expand_dims(x, axis=-1), y))
-iterator = dataset_expanded.as_numpy_iterator()
-el = next(iterator)
-print(el[0].shape)
-print(el[1].shape)
+train_expanded = train_dataset.map(lambda x, y: (tf.expand_dims(x, axis=-1), y))
+train_expanded_shuffled = train_expanded.shuffle(1000)
+train_expanded_shuffled_batched = train_expanded_shuffled.batch(128)
 
-dataset_expanded_batched = dataset_expanded.batch(128)
-iterator = dataset_expanded_batched.as_numpy_iterator()
-el = next(iterator)
-print(el[0].shape)
-print(el[1].shape)
+test_expanded = test_dataset.map(lambda x, y: (tf.expand_dims(x, axis=-1), y))
+test_expanded_shuffled = test_expanded.shuffle(1000)
+test_expanded_shuffled_batched = test_expanded_shuffled.batch(128)
+
+val_expanded = val_dataset.map(lambda x, y: (tf.expand_dims(x, axis=-1), y))
+val_expanded_shuffled = val_expanded.shuffle(1000)
+val_expanded_shuffled_batched = val_expanded_shuffled.batch(128)
 
 model_input = keras.Input(shape=(3, 3, 1), name="img")
 x = layers.Conv2D(16, 3, activation="relu")(model_input)
@@ -67,11 +101,50 @@ model.summary()
 model.compile(
     loss=tf.losses.MeanSquaredError(),
     optimizer=keras.optimizers.RMSprop(),
-    metrics=["accuracy"],
+    metrics=["mse"],
 )
 
-model.fit(dataset_expanded_batched, epochs=30)
+history = model.fit(x=train_expanded_shuffled_batched, validation_data=val_expanded_shuffled_batched, epochs=200)
 
+def plotHistory(history, metrics = [], plot_validation=False):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    for metric in metrics:
+        plt.plot(history.history[metric], label=metric)
+        if plot_validation:
+            plt.plot(history.history["val_"+metric], label="val_"+metric)
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Metric")
+    ax1.legend(loc='lower right')
+    ax2 = fig.add_subplot(212)
+    plt.plot(history.history["loss"], label="loss")
+    if plot_validation:
+        plt.plot(history.history["val_loss"], label="val_loss")
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Loss")
+    ax2.legend(loc='lower right')
+
+plotHistory(history, metrics=["mse"], plot_validation=True)
+
+test_loss, test_mse = model.evaluate(test_expanded_shuffled_batched, verbose=2)
+
+test_predictions = model.predict(test_expanded_shuffled_batched)
+test_labels = []
+for _, labels in test_expanded_shuffled_batched:  # only take first element of dataset
+    test_labels.append(labels.numpy())
+test_labels = np.vstack(test_labels)
+plt.figure()
+plt.axes(aspect='equal')
+plt.scatter(test_labels[:,0], test_predictions[:,0])
+plt.xlabel('True Values')
+plt.ylabel('Predictions')
+plt.figure()
+plt.axes(aspect='equal')
+plt.scatter(test_labels[:,1], test_predictions[:,1])
+plt.xlabel('True Values')
+plt.ylabel('Predictions')
+
+plt.show()
 
 
 
